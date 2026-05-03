3b4a27f79c1e36fa0744af273961d2d8bfb7aced
rayen <rapriadi706@gmail.com>
2026-05-03 19:45:52 +0700

    Menambahkan API Film

---
+++ Film.cs
@@ -0,0 +1,17 @@
+﻿namespace TP_MODUL10_103022400071
+{
+    public class Film
+    {
+        public string Judul { get; set; }
+        public string Sutradara { get; set; }
+        public int Tahun { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        public Film()
+        {
+
+        }
+    }
+
+}
--- obj/Debug/net10.0/TP_MODUL10_103022400071.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL10_103022400071.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL10_103022400071")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4dc94ce3d0c373981ba413dcee6e12371d58a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL10_103022400071")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL10_103022400071")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
